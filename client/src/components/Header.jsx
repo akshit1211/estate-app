fd48e21fcc61167be02c67370e615135a3473806
akshit <trehana12@gmail.com>
2024-07-22 12:13:52 +0530

    adding onChange and onSubmit functionality to the search page and complete listing item component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -27,7 +27,6 @@ export default function Listing() {
   const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
 
   useEffect(() => {
     const fetchListing = async () => {
